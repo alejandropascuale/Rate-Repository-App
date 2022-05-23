f33e5cb574068a07ed4d282154c2d65da75e1c95
alejandropascuale <apascuale@outlook.com>
2022-05-23 10:13:46 -0300

    render RepositoryItem

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import RepositoryItem from './src/components/RepositoryItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My first app with Expo</Text>
-      <StatusBar style="auto" />
+      <RepositoryItem />
     </View>
   );
 }
