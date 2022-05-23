67c95c641a1a936feed467bde0e9fdf2a83c6194
alejandropascuale <apascuale@outlook.com>
2022-05-23 17:21:53 -0300

    add theme style to item repository

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import SyledText from './StyledText'
 
 export default function RepositoryItem({ repo }) {
   return (
     <View key={repo.id} style={{ padding: 20, paddingBottom: 5, paddingTop:5 }}>
-        <Text>Full Name: {repo.fullName}</Text>
-        <Text>Description: {repo.description}</Text>
-        <Text>Language: {repo.language}</Text>
-        <Text>Stars: {repo.stargazersCount}</Text>
-        <Text>Forks: {repo.forksCount}</Text>
-        <Text>Review: {repo.reviewCount}</Text>
-        <Text>Rating: {repo.ratingAverage}</Text>
+        <SyledText fontSize='subheading' fontWeight='bold'>{repo.fullName}</SyledText>
+        <SyledText>{repo.description}</SyledText>
+        <SyledText>{repo.language}</SyledText>
+        <SyledText>Stars: {repo.stargazersCount}</SyledText>
+        <SyledText>Forks: {repo.forksCount}</SyledText>
+        <SyledText>Review: {repo.reviewCount}</SyledText>
+        <SyledText>Rating: {repo.ratingAverage}</SyledText>
     </View>
   )
 }
