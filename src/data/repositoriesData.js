7bc04a7a537bc32d906a4a153230fced8a5bf487
alejandropascuale <apascuale@outlook.com>
2022-05-23 11:45:44 -0300

    create Repository data to render

---
+++ src/data/repositoriesData.js
@@ -0,0 +1,134 @@
+export default [
+    {
+        id: 1,
+        fullName: 'MyChatApp',
+        description: 'Project created with Swift',
+        language: 'Swift',
+        forksCount: 123,
+        stargazersCount: 4168,
+        ratingAverage: 5.4,
+        reviewCount: 4,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/mEZ3PoFGs_k'
+    },
+        {
+        id: 2,
+        fullName: 'WeatherApp',
+        description: '',
+        language: 'Swift',
+        forksCount: 45,
+        stargazersCount: 481,
+        ratingAverage: 7.1,
+        reviewCount: 1,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/WNoLnJo7tS8'
+    },
+        {
+        id: 3,
+        fullName: 'Netflix-Clone',
+        description: 'Netflix Clone with React',
+        language: 'JavaScript',
+        forksCount: 761,
+        stargazersCount: 412,
+        ratingAverage: 8.2,
+        reviewCount: 61,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/ZHvM3XIOHoE'
+    },
+        {
+        id: 4,
+        fullName: 'todoApp',
+        description: '',
+        language: 'JavaScript',
+        forksCount: 78,
+        stargazersCount: 989,
+        ratingAverage: 6.4,
+        reviewCount: 1,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/C8Ta0gwPbQg'
+    },
+        {
+        id: 5,
+        fullName: 'walletApp',
+        description: 'personal wallet to organize income and expenses',
+        language: 'JavaScript',
+        forksCount: 6152,
+        stargazersCount: 7456,
+        ratingAverage: 6.9,
+        reviewCount: 687,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/iEEBWgY_6lA'
+    },
+        {
+        id: 6,
+        fullName: 'APchat',
+        description: '',
+        language: 'JavaScript',
+        forksCount: 11,
+        stargazersCount: 56,
+        ratingAverage: 4.3,
+        reviewCount: 0,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/MTZTGvDsHFY'
+    },
+        {
+        id: 7,
+        fullName: 'Barber-App',
+        description: 'React Native Barber App',
+        language: 'Java',
+        forksCount: 45,
+        stargazersCount: 7,
+        ratingAverage: 5.1,
+        reviewCount: 1,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/IF9TK5Uy-KI'
+    },
+        {
+        id: 8,
+        fullName: 'Api_Rest',
+        description: 'Project with Python and Django',
+        language: 'Python',
+        forksCount: 87,
+        stargazersCount: 63,
+        ratingAverage: 6.5,
+        reviewCount: 7,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/rDEOVtE7vOs'
+    },
+        {
+        id: 9,
+        fullName: 'Bithday-Reminder',
+        description: 'Birthday Reminder in React',
+        language: 'Javascript-CSS',
+        forksCount: 2,
+        stargazersCount: 10,
+        ratingAverage: 6,
+        reviewCount: 0,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/6anudmpILw4'
+    },
+        {
+        id: 10,
+        fullName: 'Comamos_app',
+        description: 'Project to reserve tables and products in restaurants',
+        language: 'JavaScript-EJS',
+        forksCount: 10547,
+        stargazersCount: 91,
+        ratingAverage: 7.9,
+        reviewCount: 486,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/0J9l9xRyMSo'
+    },
+        {
+        id: 11,
+        fullName: 'MercadoLiebre-Nuevo',
+        description: 'Nuevo ejercicio de Mercado Liebre',
+        language: 'JavaScript',
+        forksCount: 23,
+        stargazersCount: 11,
+        ratingAverage: 6.2,
+        reviewCount: 2,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/DItYlc26zVI'
+    },
+        {
+        id: 12,
+        fullName: 'Tareas-app',
+        description: 'Aplicación de Tareas para ejercicio',
+        language: 'JavaScript',
+        forksCount: 2,
+        stargazersCount: 3,
+        ratingAverage: 4,
+        reviewCount: 0,
+        ownerAvatarUrl: 'https://unsplash.com/es/fotos/iFgRcqHznqg'
+    },
+]
