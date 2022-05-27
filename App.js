91e850595568e4d3d938fd5be773f59970946847
alejandropascuale <apascuale@outlook.com>
2022-05-27 16:50:53 -0300

    new add Appbar in main

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import Main from './src/components/Main.jsx'
-import { NativeRouter } from 'react-router-native'
-
 
 export default function App() {
   return (
-    <NativeRouter>
-      <Main />
-    </NativeRouter>
+    <Main />
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Constants from 'expo-constants'
 import { View } from 'react-native'
+import AppBar from './AppBar'
 import RepositoryList from './RepositoryList'
 
 export default function Main () {
   return (
     <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
-        <RepositoryList />
+      <AppBar />
+      <RepositoryList />
     </View>
   )
 }
