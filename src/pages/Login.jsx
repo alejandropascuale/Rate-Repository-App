6787c76d246a9da5dea77cd4c4ab47d375600792
alejandropascuale <apascuale@outlook.com>
2022-06-01 18:26:23 -0300

    fill Login component with formik data

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Formik } from 'formik'
+import { Button, TextInput, View } from 'react-native'
+
+const initialValues = {
+  email: '',
+  password: ''
+}
 
 export default function Login() {
   return (
-    <>
-        
-    </>
+    <Formik initialValues={initialValues} onSubmit={values => console.log(values)}>
+        {({ handleChange, handleSubmit, values }) => {
+          return (
+            <View>
+              <TextInput placeholder='Email' value={values.email} onChangeText={handleChange('email')} />
+              <TextInput placeholder='Password' value={values.password} onChangeText={handleChange('password')} />
+              <Button title='Log in' onPress={handleSubmit} ></Button>
+            </View>
+          )
+        }}
+    </Formik>
   )
 }
