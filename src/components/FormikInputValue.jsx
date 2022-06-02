381dc1f92bdd72e173cf238865683a2c9b4d526b
alejandropascuale <apascuale@outlook.com>
2022-06-02 11:49:28 -0300

    show errors in screen and add style error

--- src/components/FormikInputValue.jsx
+++ src/components/FormikInputValue.jsx
@@ -1,14 +1,28 @@
 import { useField } from 'formik'
 import React from 'react'
 import StyledTextInput from './StyledTextInput'
+import StyledText from './StyledText'
+import { StyleSheet } from 'react-native'
 
 export default function FormikInputValue({ name, ...props }) {
     const [field, meta, helpers] = useField(name)
   return (
-    <StyledTextInput 
-        value={field.value} 
-        onChangeText={value => helpers.setValue(value)} 
-        { ...props }
-    />
+      <>
+        <StyledTextInput 
+            value={field.value} 
+            onChangeText={value => helpers.setValue(value)} 
+            { ...props }
+        />
+        { meta.error && <StyledText style={styles.error}>{ meta.error }</StyledText> }
+      </>
   )
 }
+
+const styles = StyleSheet.create({
+    error: {
+        color: 'red',
+        fontSize: 10,
+        marginBottom: 20,
+        marginTop: -5
+    }
+})
